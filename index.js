f6d2085e6b9ab13d2de350c9f9eca5362ba84438
Kayla C <itskaychay@gmail.com>
2023-10-30 16:58:53 -0400

    updated

--- index.js
+++ index.js
@@ -30,26 +30,7 @@ const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
 //   })
 // );
 
-let allowedOrigins = [
-  "http://localhost:8080",
-  "https://myflix-app-kc.netlify.app",
-];
-
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin isn’t found on the list of allowed origins
-        let message =
-          "The CORS policy for this application doesn’t allow access from origin " +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+app.use(cors());
 
 app.use(morgan("common", { stream: accessLogStream })); // Use morgan as middleware to log requests to the log file.
 app.use(express.static("public")); // Set the "public" folder as a static folder to serve static files.
